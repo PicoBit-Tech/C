31e69dc47cbc83356257d7ac08cc4e49b37af54d
PicoBit-Tech <picobitinfo@gmail.com>
2025-03-06 20:51:51 +0330

    Data Type in C

---
+++ PicoBit-Tech/src/PicoBit-Tech.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : PicoBit-Tech.c
+ Author      : Behnam Jafari Touchaei
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main(void) {
+	uint32_t varU8;
+	printf("test:%lld",sizeof(varU8));
+	printf("test:%lld",sizeof(varU8));
+	return 0;
+}
